6aff6d1e94fa369404b814845fae845424a0a855
OrgovanGeza <orgo.geza@gmail.com>
2021-06-21 17:17:31 +0100

    solved

---
+++ week-02/day-01/Functions/print-arguments.ts
@@ -0,0 +1,12 @@
+'use strict';
+export {};
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams(inpVal1:any,inpVal2?:any,inpVal3?:any,inpVal4?:any,inpVal5?:any,inpVal6?:any,){
+    console.log(inpVal1,inpVal2,inpVal3,inpVal4,inpVal5,inpVal6);
+}
+
+printParams(3,'jhgf',true)
