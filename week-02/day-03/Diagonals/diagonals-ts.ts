6f4d2d231d4ad529973e929475eca1d52b158a26
OrgovanGeza <orgo.geza@gmail.com>
2021-06-23 13:36:20 +0100

    solved

--- week-02/day-03/Diagonals/diagonals-ts.ts
+++ week-02/day-03/Diagonals/diagonals-ts.ts
@@ -13,15 +13,41 @@ export {};
 // Make decision about the color in the function
 
 
-const xStart = 10;
-const yStart = 10;
+const xStart = 0;
+const yStart = 0;
+
+const canvasWidth = 600;
+const canvasHeight = 400;
 
 function drawDiagonal (xStart : number, yStart: number){
+    let cornAx = 0;
+    let cornAy = 0;
+    let cornBx = canvasWidth;
+    let cornBy = 0;
+    let cornCx = canvasWidth;
+    let cornCy = canvasHeight;
+    let cornDx = 0;
+    let cornDy = canvasHeight;
+
     ctx.beginPath();
     ctx.strokeStyle = ((xStart==0 && yStart==0) ? "green" : "red");
-    ctx.moveTo(xStart, yStart);
-    ctx.lineTo(xStart+50, yStart+50);
-    ctx.stroke();
+        if (xStart==cornAx && yStart==cornAy){
+            ctx.moveTo(cornAx, cornAy);
+            ctx.lineTo(cornCx, cornCy);
+            ctx.stroke();
+        } else if (xStart==cornBx && yStart==cornBy){
+            ctx.moveTo(cornBx, cornBy);
+            ctx.lineTo(cornDx, cornDy);
+            ctx.stroke();
+        } else if (xStart==cornCx && yStart==cornCy){
+            ctx.moveTo(cornCx, cornCy);
+            ctx.lineTo(cornAx, cornAy);
+            ctx.stroke();
+        } else if (xStart==cornDx && yStart==cornDy){
+            ctx.moveTo(cornDx, cornDy);
+            ctx.lineTo(cornBx, cornBy);
+            ctx.stroke();
+        }
 }
 
 drawDiagonal(xStart,yStart);
