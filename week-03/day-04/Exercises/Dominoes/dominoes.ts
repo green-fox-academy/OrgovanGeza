5db03ac93a4becca6f70b0b6acd00f12bf94393b
OrgovanGeza <orgo.geza@gmail.com>
2021-07-01 16:15:14 +0100

    if function created to compare and switch 2 dominoes

---
+++ week-03/day-04/Exercises/Dominoes/dominoes.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+import {Domino} from "./domino";
+
+function initializeDominoes(): Domino[] {
+    let dominoes = [];
+    dominoes.push(new Domino(5, 2));
+    dominoes.push(new Domino(4, 6));
+    dominoes.push(new Domino(1, 5));
+    dominoes.push(new Domino(6, 7));
+    dominoes.push(new Domino(2 ,4));
+    dominoes.push(new Domino(7, 1));
+    return dominoes;
+}
+
+function print(dominoes: Domino[]) {
+    dominoes.forEach(function (value) {
+        console.log(value);
+    });
+}
+
+let dominoes = initializeDominoes();
+/** You have the list of Dominoes */
+/** Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides */
+/** eg: [2, 4], [4, 3], [3, 5] ... */
+
+
+
+let tempItem : Domino;
+
+
+if (dominoes[0].values[1] !== dominoes[1].values[0]){
+    tempItem = dominoes[0];
+    dominoes[0] = dominoes[1];
+    dominoes[1] = tempItem;
+}
+
+
+print(dominoes);
