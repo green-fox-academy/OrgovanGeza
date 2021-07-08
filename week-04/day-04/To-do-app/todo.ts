c3de07140b0aa73b93c5c351a3fa2c3ad78b6f55
OrgovanGeza <orgo.geza@gmail.com>
2021-07-08 12:31:07 +0100

    solved till 'add new task'

---
+++ week-04/day-04/To-do-app/todo.ts
@@ -0,0 +1,52 @@
+'use strict';
+
+//import .....
+
+const args: string[] = process.argv.slice(2);
+const fs = require('fs');
+const todoFile = 'todo.txt';
+
+let usageInfos = (): void => {
+    console.log('Command Line Todo application');
+    console.log('=============================');
+    console.log('');
+    console.log('Command line arguments:');
+    console.log('    -l   Lists all the tasks');
+    console.log('    -a   Adds a new task');
+    console.log('    -r   Removes an task');
+    console.log('    -c   Completes an task');
+}
+
+let listItems = (): void => {
+    const fileItems = fs.readFileSync(todoFile, 'utf-8').split('\n');
+
+    if (fileItems.length === 1 && fileItems[0] === '') {
+        console.log('No todos for today! :)');
+    } else {
+        for (let i = 0; i < fileItems.length; i++) {
+            console.log(i + 1, ' - ', fileItems[i]);
+        }
+    }
+}
+
+
+let addTask = (taskToBeAdded: string): void => {
+    const fileItems = fs.readFileSync(todoFile, 'utf-8');
+    if (fileItems === '') {
+        fs.writeFileSync(todoFile, taskToBeAdded);
+    } else {
+        const extendedList = fileItems + '\n' + taskToBeAdded;
+        fs.writeFileSync(todoFile, extendedList);
+    }
+}
+
+
+
+
+if (args.length === 0) {
+    usageInfos();
+} else if (args[0] === '-l') {
+    listItems();
+} else if (args[0] == '-a') {
+    addTask(args[1]);
+}
