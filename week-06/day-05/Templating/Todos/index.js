59a42aeca778b947a7ca83a9521737dc2a4e4fbb
OrgovanGeza <orgo.geza@gmail.com>
2021-07-31 18:28:16 +0100

    solved- looping on an array in home.ejs, adding the array in home.js

--- week-06/day-05/Templating/Todos/index.js
+++ week-06/day-05/Templating/Todos/index.js
@@ -3,13 +3,14 @@ import ejs from 'ejs';
 
 const app = express();
 const PORT = 3000;
+const todos = ['get up', 'survive', 'go back to bed'];
 
 app.set('view engine', 'ejs');
 
 app.get('/', todo);
 
 function todo(req, res) {
-    
+    res.render('home', {todos})
 }
 
 app.listen(PORT, () => {
