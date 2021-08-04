00f2104cd1184db9811e85d69ca2eb6d1064af22
OrgovanGeza <orgo.geza@gmail.com>
2021-08-04 15:38:56 +0100

    multiple tables joined and pushed to frontend, dinamic table creation on frontend

--- week-07/day-03/Bookstore/index.js
+++ week-07/day-03/Bookstore/index.js
@@ -34,7 +34,27 @@ app.get('/books', (req, res) => {
         // res.json(result); //if i want to send a json response, without a frontend
 
     })
+
 })
+
+app.get('/books/full', (req, res) => {
+    conn.query(`
+        SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
+        FROM bookstore.book_mast 
+        INNER JOIN author ON bookstore.book_mast.aut_id = bookstore.author.aut_id
+        INNER JOIN category ON bookstore.book_mast.cate_id = bookstore.category.cate_id
+        INNER JOIN publisher ON bookstore.book_mast.pub_id = bookstore.publisher.pub_id;`
+        , (err, result) => {
+            if (err) {
+                console.log(err);
+                res.send(500); // automatically returns
+            }
+            res.status(200).render('homeFull', { fullBooks: result })
+            // res.json(result); //if i want to send a json response, without a frontend
+
+        })
+})
+
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
