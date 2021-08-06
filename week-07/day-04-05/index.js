92501bfccf93df1b2d6f1c55bcb668ad3356375a
OrgovanGeza <orgo.geza@gmail.com>
2021-08-06 15:34:38 +0100

    DELETE requesrt with the requiered response implemented

--- week-07/day-04-05/index.js
+++ week-07/day-04-05/index.js
@@ -18,9 +18,9 @@ const conn = mysql.createConnection({
     database: 'reddit'
 })
 
-function sendError(err) {
+function sendError(err, res) {
     console.log(err);
-    res.send(500);
+    res.sendStatus(500);
 }
 
 conn.connect((err) => {
@@ -28,14 +28,14 @@ conn.connect((err) => {
         console.error('Cannot connect to database,', err);
         return;
     }
-    console.log('Connection is as strong as a badger');
+    console.log('MySQL connection is as strong as a badger');
 })
 
 app.get('/posts', (req, res) => {
 
     conn.query(`SELECT * FROM reddit.posts;`, (err, posts) => {
         if (err) {
-            sendError(err);
+            sendError(err, res);
         }
         res.status(200).json({ posts });
     })
@@ -50,7 +50,7 @@ app.post('/posts', (req, res) => {
     conn.query(`INSERT INTO posts (title, url, timestamp) 
                VALUES (?,?,?);`, [`${newPost.title}`, `${newPost.url}`, `${newPost.timestamp}`], (err, result) => {
         if (err) {
-            sendError(err);
+            sendError(err, res);
         }
         newPost.id = result.insertId;
         res.status(200).json(newPost);
@@ -63,13 +63,13 @@ app.put('/posts/:id/upvote', (req, res) => {
 
     conn.query(`UPDATE posts SET score = score +1 WHERE (id = ${upvotedPostId});`, (err) => {
         if (err) {
-            sendError(err);
+            sendError(err, res);
         }
     });
 
     conn.query(`SELECT * FROM posts WHERE id = ${upvotedPostId};`, (err, result) => {
         if (err) {
-            sendError(err);
+            sendError(err, res);
         }
         res.status(200).json(result[0])
     })
@@ -81,17 +81,41 @@ app.put('/posts/:id/downvote', (req, res) => {
 
     conn.query(`UPDATE posts SET score = score -1 WHERE (id = ${downvotedPostId});`, (err) => {
         if (err) {
-            sendError(err);
+            sendError(err, res);
         }
     });
 
     conn.query(`SELECT * FROM posts WHERE id = ${downvotedPostId};`, (err, result) => {
         if (err) {
-            sendError(err);
+            sendError(err, res);
         }
         res.status(200).json(result[0])
     })
 
+})
+
+app.delete('/posts/:id', (req, res) => {
+    const deletedPost = {
+        id: req.params.id
+    }
+
+    conn.query(`SELECT * FROM posts WHERE id = ${deletedPost.id};`, (err, result) => {
+        if (err) {
+            sendError(err, res);
+        }
+        deletedPost.title = result[0].title;
+        deletedPost.url = result[0].url;
+        deletedPost.timestamp = result[0].timestamp;
+        deletedPost.score = result[0].score;
+    })
+
+    conn.query(`DELETE from posts WHERE id = ${deletedPost.id};`, (err) => {
+        if (err) {
+            sendError(err, res);
+        }
+        res.status(200).json(deletedPost);
+    })
+    
 })
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
